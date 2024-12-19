243fbb8a09b984eff7b44c8eae3f894bbde8e23b
K0O7 <57181698+K0O7@users.noreply.github.com>
2024-12-19 13:17:33 +0100

    simple spawner and scene for testing

---
+++ scripts/spawner.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+
+@export var time_to_spawn: float = 5.
+@export var spawn_location_offset_X: int = 0
+@export var spawn_location_offset_Y: int = 0
+@export var entity_to_spawn: PackedScene
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.wait_time = time_to_spawn
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	var new_entity = entity_to_spawn.instantiate()
+	add_sibling(new_entity)
+	new_entity.global_position.x = global_position.x+spawn_location_offset_X
+	new_entity.global_position.y = global_position.x+spawn_location_offset_Y
+	pass # Replace with function body.
